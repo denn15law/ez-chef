80f746be914e5cbfaa2fbd6e916e4a54d826576c
Jeewon Lee <jeewon.jlee@gmail.com>
2022-01-14 12:57:59 -0500

    Backend setup

---
+++ backend/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ezchef-cluster.re5dq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+};
