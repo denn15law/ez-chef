ea3f48ab4f8d02af03ba30c1be15ca4209be05f0
Jeewon Lee <jeewon.jlee@gmail.com>
2022-01-14 15:10:48 -0500

    Frontend setup

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 8000;
+
 app.use(bodyParser.json());
+dotenv.config();
 
 // Database config
 const db = require("./config/keys").mongoURI;
@@ -15,7 +17,5 @@ mongoose
   .connect(db)
   .then(() => console.log("MongoDB connected!"))
   .catch((err) => console.log(err));
-
-const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
