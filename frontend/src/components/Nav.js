3945078dc8252567dbb46db81bdceb2ca8d7611e
Jeewon Lee <jeewon.jlee@gmail.com>
2022-01-14 23:55:17 -0500

    Use MUI template for navbar

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,35 +1,181 @@
-import React from "react";
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 import { Link } from "react-router-dom";
 
-const Nav = (props) => {
+const drawerWidth = 240;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+const Nav = () => {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <nav>
-      <h3>Nav Bar</h3>
-      <ul className="nav-links">
-        <Link to="/">
-          <li>Home Page</li>
-        </Link>
-        <Link to="/myrecipes">
-          <li>My Recipes</li>
-        </Link>
-        <Link to="/new">
-          <li>Add New Recipe</li>
-        </Link>
-        <Link to="/favourites">
-          <li>My Favourites</li>
-        </Link>
-        <Link to="/grocerylist">
-          <li>Grocery List</li>
-        </Link>
-        <Link to="/register">
-          <li>Register</li>
-        </Link>
-        <Link to="/login">
-          <li>Login</li>
-        </Link>
-      </ul>
-    </nav>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: "none" }) }}>
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {[
+            "Home",
+            "My Recipes",
+            "Add Recipe",
+            "My Favourites",
+            "Grocery List",
+          ].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          <Link to="/register">
+            <li>Register</li>
+          </Link>
+          <Link to="/login">
+            <li>Login</li>
+          </Link>
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+      </Main>
+    </Box>
   );
 };
 
 export default Nav;
+
+// const Nav = (props) => {
+//   return (
+//     <nav>
+//       <h3>Nav Bar</h3>
+//       <ul className="nav-links">
+//         <Link to="/">
+//           <li>Home Page</li>
+//         </Link>
+//         <Link to="/myrecipes">
+//           <li>My Recipes</li>
+//         </Link>
+//         <Link to="/new">
+//           <li>Add New Recipe</li>
+//         </Link>
+//         <Link to="/favourites">
+//           <li>My Favourites</li>
+//         </Link>
+//         <Link to="/grocerylist">
+//           <li>Grocery List</li>
+//         </Link>
+//         <Link to="/register">
+//           <li>Register</li>
+//         </Link>
+//         <Link to="/login">
+//           <li>Login</li>
+//         </Link>
+//       </ul>
+//     </nav>
+//   );
+// };
