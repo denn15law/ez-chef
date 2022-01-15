67af913bd660962bd43b8e977d4eac1c8dce8403
dennis law <denn15_law@live.ca>
2022-01-14 23:30:40 -0500

    remove unused code and add NewRecipe component template

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,10 @@
 import "../App.css";
+import NewRecipe from "./NewRecipe";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <NewRecipe />
     </div>
   );
 };
