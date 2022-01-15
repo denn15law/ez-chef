a636bb44e46770c35f133f4df8c78c4d5e517537
Jeewon Lee <jeewon.jlee@gmail.com>
2022-01-15 15:47:32 -0500

    Install axios and revise New Recipe Form

--- frontend/src/components/RecipeForm.js
+++ frontend/src/components/RecipeForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Container, Box, TextField, Input } from "@mui/material";
+import axios from "axios";
 
 class RecipeForm extends Component {
   constructor(props) {
@@ -43,86 +44,102 @@ class RecipeForm extends Component {
     });
   };
 
-  handleMeasurementChange = (event) => {
-    this.setState({
-      ingredients: {
-        measurement: event.target.value,
-      },
-    });
-  };
+  // handleMeasurementChange = (event) => {
+  //   this.setState({
+  //     ingredients: {
+  //       measurement: event.target.value,
+  //     },
+  //   });
+  // };
 
-  handleIngredientNameChange = (event) => {
-    this.setState({
-      ingredients: {
-        name: event.target.value,
-      },
-    });
+  // handleIngredientNameChange = (event) => {
+  //   this.setState({
+  //     ingredients: {
+  //       name: event.target.value,
+  //     },
+  //   });
+  // };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const data = this.state;
+    axios
+      .post("/recipes", data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   render() {
     return (
-      <Container>
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "25ch" },
-          }}>
-          <div>
-            <Input
-              required
-              id="standard-basic"
-              label="title"
-              placeholder="Title"
-              value={this.state.title}
-              onChange={this.handleTitleChange}
-            />
-          </div>
-          <div>
-            <Input
-              required
-              id="standard-basic"
-              label="image-url"
-              placeholder="Image URL"
-              value={this.state.title}
-              onChange={this.handleTitleChange}
-            />
-          </div>
-          <div>
-            <TextField
-              required
-              multiline
-              id="outlined-multiline-flexible"
-              label="Instructions"
-              type="text"
-              value={this.state.instruction}
-              onChange={this.handleInstructionChange}
-            />
-          </div>
-          <div>
-            <label>Serving Size</label>
-            <input
-              type="number"
-              value={this.state.serving_size}
-              onChange={this.handleServingSizeChange}
-              min={0}
-            />
-          </div>
-          <div>
-            <label>Ingredients</label>
-            <input
-              type="number"
-              value={this.state.ingredients["measurement"]}
-              onChange={this.handleMeasurementChange}
-              min={0}
-            />
-            <input
-              type="text"
-              value={this.state.ingredients["name"]}
-              onChange={this.handleIngredientNameChange}
-            />
-          </div>
-        </Box>
-      </Container>
+      <form onSubmit={this.handleSubmit}>
+        <Container>
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { m: 1, width: "25ch" },
+            }}>
+            <div>
+              <Input
+                required
+                id="standard-basic"
+                label="title"
+                placeholder="Title"
+                value={this.state.title}
+                onChange={this.handleTitleChange}
+              />
+            </div>
+            <div>
+              <Input
+                required
+                id="standard-basic"
+                label="image-url"
+                placeholder="Image URL"
+                value={this.state.image_url}
+                onChange={this.handleURLChange}
+              />
+            </div>
+            <div>
+              <TextField
+                required
+                multiline
+                id="outlined-multiline-flexible"
+                label="Instructions"
+                type="text"
+                value={this.state.instruction}
+                onChange={this.handleInstructionChange}
+              />
+            </div>
+            <div>
+              <label>Serving Size</label>
+              <input
+                type="number"
+                value={this.state.serving_size}
+                onChange={this.handleServingSizeChange}
+                min={0}
+              />
+            </div>
+            {/* <div>
+              <label>Ingredients</label>
+              <input
+                type="number"
+                value={this.state.ingredients["measurement"]}
+                onChange={this.handleMeasurementChange}
+                min={0}
+              />
+              <input
+                type="text"
+                value={this.state.ingredients["name"]}
+                onChange={this.handleIngredientNameChange}
+              />
+            </div> */}
+            <button type="submit">Create</button>
+          </Box>
+        </Container>
+      </form>
     );
   }
 }
