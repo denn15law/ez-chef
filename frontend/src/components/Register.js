5f74b57ee1a4e8b25275ee98fbb69507e29c33e7
Jeewon Lee <jeewon.jlee@gmail.com>
2022-01-16 00:42:29 -0500

    Re-add page styling

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 class Register extends Component {
   constructor(props) {
@@ -68,53 +79,114 @@ class Register extends Component {
   }
 
   render() {
+    const Copyright = (props) => {
+      return (
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          align="center"
+          {...props}>
+          {"Copyright © "}
+          <Link color="inherit" href="https://mui.com/">
+            Your Website
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      );
+    };
+
+    const theme = createTheme();
+
     return (
-      <div>
-        <h3>Create New User</h3>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <div>
-              <label>First Name: </label>
-              <input
-                type="text"
-                required
-                className="form-control"
-                value={this.state.first_name}
-                onChange={this.onChangeFirstName}
-              />
-              <label>Last Name: </label>
-              <input
-                type="text"
-                required
-                className="form-control"
-                value={this.state.last_name}
-                onChange={this.onChangeLastName}
-              />
-            </div>
-            <div>
-              <label>Email Address: </label>
-              <input
-                type="email"
-                required
-                className="form-control"
-                value={this.state.email}
-                onChange={this.onChangeEmail}
-              />
-              <label>Password: </label>
-              <input
-                type="password"
-                required
-                className="form-control"
-                value={this.state.password}
-                onChange={this.onChangePassword}
-              />
-            </div>
-          </div>
-          <div className="form-group">
-            <input type="submit" value="Create" className="btn btn-primary" />
-          </div>
-        </form>
-      </div>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <AppRegistrationIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up for an Account
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={this.onSubmit}
+              sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                    value={this.state.first_name}
+                    onChange={this.onChangeFirstName}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    name="lastName"
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    autoFocus
+                    value={this.state.last_name}
+                    onChange={this.onChangeLastName}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="email"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    type="email"
+                    autoComplete="email"
+                    value={this.state.email}
+                    onChange={this.onChangeEmail}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="password"
+                    required
+                    fullWidth
+                    id="password"
+                    label="Password"
+                    type="password"
+                    autoComplete="new-password"
+                    value={this.state.password}
+                    onChange={this.onChangePassword}
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}>
+                Sign Up
+              </Button>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 5 }} />
+        </Container>
+      </ThemeProvider>
     );
   }
 }
