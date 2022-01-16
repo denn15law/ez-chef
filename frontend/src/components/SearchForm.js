cc94f898c994436ca24e1e1e18c79b303418703d
Kyle <kliang1194@gmail.com>
2022-01-16 10:34:20 -0500

    Clear input after search and display search terms under search bar

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,5 +1,4 @@
-import React, { Component, useState, Fragment } from "react";
-import RecipeList from "./RecipeList";
+import React, { useState } from "react";
 import axios from "axios";
 
 const SearchForm = () => {
@@ -7,10 +6,16 @@ const SearchForm = () => {
   const [search, setSearch] = useState("");
 
   function handleChange(e) {
-    setSearch(e.target.value.replaceAll(",", "+").replaceAll(", ", "+"));
+    setSearch(
+      e.target.value
+        .replaceAll(",", "+")
+        .replaceAll(", ", "+")
+        .replaceAll(" ", "")
+    );
   }
 
   const getSearch = () => {
+    console.log(search);
     axios
       .get(
         `https://api.spoonacular.com/recipes/complexSearch?apiKey=509c30240911425fa631c29ca02b8e8c&query=${search}&number=20`
@@ -24,17 +29,21 @@ const SearchForm = () => {
         // handle error
         console.log(error);
       });
+
+    document.getElementById("mainInput").value = "";
   };
 
   return (
     <div>
       <h1 className="Search-Form">Recipe Search</h1>
       <input
+        id="mainInput"
         type="text"
         placeholder="Enter Ingredients or Keywords"
         onChange={handleChange}
       />
       <button onClick={getSearch}>Search Recipe</button>
+      <h4>Now displaying recipes containing: {search.replaceAll("+", ", ")}</h4>
       {recipeData.length
         ? recipeData.map((recip) => {
             return (
