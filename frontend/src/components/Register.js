4fac1c4bb4329698915a4fa03aea445f146260cb
Jeewon Lee <jeewon.jlee@gmail.com>
2022-01-16 00:24:40 -0500

    Add registration form and use axios to add user data to database

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,130 +1,122 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React, { Component } from "react";
+import axios from "axios";
 
-const Copyright = (props) => {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}>
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-};
+class Register extends Component {
+  constructor(props) {
+    super(props);
 
-const theme = createTheme();
+    this.onChangeFirstName = this.onChangeFirstName.bind(this);
+    this.onChangeLastName = this.onChangeLastName.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-const Register = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+    this.state = {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+    };
+  }
+
+  onChangeFirstName(e) {
+    this.setState({
+      first_name: e.target.value,
     });
-  };
+  }
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}>
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <AppRegistrationIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up for an Account
-          </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}>
-              Sign Up
-            </Button>
-            <Link href="/login" variant="body2">
-              Already have an account? Sign in
-            </Link>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
-  );
-};
+  onChangeLastName(e) {
+    this.setState({
+      last_name: e.target.value,
+    });
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    console.log(user);
+
+    axios
+      .post("http://localhost:8000/users/", user)
+      .then((res) => console.log(res.data));
+
+    this.setState({
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <div>
+              <label>First Name: </label>
+              <input
+                type="text"
+                required
+                className="form-control"
+                value={this.state.first_name}
+                onChange={this.onChangeFirstName}
+              />
+              <label>Last Name: </label>
+              <input
+                type="text"
+                required
+                className="form-control"
+                value={this.state.last_name}
+                onChange={this.onChangeLastName}
+              />
+            </div>
+            <div>
+              <label>Email Address: </label>
+              <input
+                type="email"
+                required
+                className="form-control"
+                value={this.state.email}
+                onChange={this.onChangeEmail}
+              />
+              <label>Password: </label>
+              <input
+                type="password"
+                required
+                className="form-control"
+                value={this.state.password}
+                onChange={this.onChangePassword}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Register;
