1ee3826f72090f5781b896d1532cbf3323c2493f
Kyle <kliang1194@gmail.com>
2022-01-16 23:52:45 -0500

    List ingredients and recipe instructions on recipe details

--- frontend/src/components/RecipeDetails.js
+++ frontend/src/components/RecipeDetails.js
@@ -11,6 +11,7 @@ const RecipeDetails = () => {
     axios
       .get(`http://localhost:8000/search/id/${id}`)
       .then(function (response) {
+        console.log("iamdata", response.data);
         setDetails(response.data);
       })
       .catch(function (error) {
@@ -23,15 +24,64 @@ const RecipeDetails = () => {
     <div>
       <h1>{details.title}</h1>
       <img src={details.image}></img>
-      <p>
-        {details.instructions
-          ? details.instructions
-              .replaceAll("<li>", "")
-              .replaceAll("</li>", "")
-              .replaceAll("<ol>", "")
-              .replaceAll("</ol>", "")
-          : null}
-      </p>
+      <div id="recipe-ingredients">
+        <h3>Recipe Ingredients:</h3>
+        <ul>
+          {Object.values(details).length > 0
+            ? details.extendedIngredients.map((ing) => {
+                return (
+                  <li key={ing.id}>
+                    {ing.amount} {ing.unit} {ing.name}
+                  </li>
+                );
+              })
+            : null}
+        </ul>
+      </div>
+      <div id="recipe-instructions">
+        <h3>Cooking Instructions:</h3>
+        <ol type="1">
+          {Object.values(details).length
+            ? details.instructions
+                .replaceAll("<li>", "")
+                .replaceAll("</li>", "")
+                .replaceAll("<ol>", "")
+                .replaceAll("</ol>", "")
+                .replaceAll("<p>", "")
+                .replaceAll("</p>", "")
+                .replaceAll("<b>", "")
+                .replaceAll("</b>", "")
+                .split(".")
+                .slice(0, -1)
+                .map((each) => {
+                  return (
+                    <li
+                      key={details.instructions
+                        .replaceAll("<li>", "")
+                        .replaceAll("</li>", "")
+                        .replaceAll("<ol>", "")
+                        .replaceAll("</ol>", "")
+                        .replaceAll("<p>", "")
+                        .replaceAll("</p>", "")
+                        .replaceAll("<b>", "")
+                        .replaceAll("</b>", "")
+                        .split(".")
+                        .indexOf(each)}
+                    >
+                      {each}
+                    </li>
+                  );
+                })
+            : null}
+          {/* {details.instructions
+            ? details.instructions
+                .replaceAll("<li>", "")
+                .replaceAll("</li>", "")
+                .replaceAll("<ol>", "")
+                .replaceAll("</ol>", "")
+            : null} */}
+        </ol>
+      </div>
     </div>
   );
 };
