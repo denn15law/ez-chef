407c04174624e3f5fe7860356f1b90890c630f46
Kyle <kliang1194@gmail.com>
2022-01-19 16:52:57 -0500

    update search routes in backend

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -3,7 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 const { searchFunc, recipeDetails } = require("../controllers/search");
-router.get("/:searched", searchFunc);
+
+router.get("/results/:searched", searchFunc);
 router.get("/details/:id", recipeDetails);
 
 module.exports = router;
