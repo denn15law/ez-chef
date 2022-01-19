dec44fb632781c4817f38a234a13069fcea496d1
Jeewon Lee <jeewon.jlee@gmail.com>
2022-01-19 15:29:20 -0500

    Revert to old styling

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -9,7 +9,6 @@ import {
   CssBaseline,
   Grid,
   Link,
-  Paper,
   TextField,
   Typography,
 } from "@mui/material";
@@ -46,114 +45,91 @@ const Register = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: "100vh" }}>
+      <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
+        <Box
           sx={{
-            backgroundImage: "url(https://source.unsplash.com/random)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Paper sx={{ p: 2, margin: "auto", maxWidth: 500, flexGrow: 1 }}>
-            <Box
-              sx={{
-                p: 2,
-                margin: "auto",
-                maxWidth: 500,
-                flexGrow: 1,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}>
-              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                <AppRegistrationIcon />
-              </Avatar>
-              <Typography component="h1" variant="h5">
-                Sign up for an Account
-              </Typography>
-              <Box
-                component="form"
-                noValidate
-                onSubmit={handleSubmit}
-                sx={{ mt: 3 }}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      name="first_name"
-                      required
-                      fullWidth
-                      id="firstName"
-                      label="First Name"
-                      autoFocus
-                      value={data.first_name}
-                      onChange={handleChange}
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      name="last_name"
-                      required
-                      fullWidth
-                      id="lastName"
-                      label="Last Name"
-                      autoFocus
-                      value={data.last_name}
-                      onChange={handleChange}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      name="email"
-                      required
-                      fullWidth
-                      id="email"
-                      label="Email Address"
-                      type="email"
-                      autoComplete="email"
-                      value={data.email}
-                      onChange={handleChange}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      name="password"
-                      required
-                      fullWidth
-                      id="password"
-                      label="Password"
-                      type="password"
-                      autoComplete="new-password"
-                      value={data.password}
-                      onChange={handleChange}
-                    />
-                  </Grid>
-                </Grid>
-                <Button
-                  type="submit"
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <AppRegistrationIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up for an Account
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="first_name"
+                  required
                   fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}>
-                  Sign Up
-                </Button>
-                <Link href="/login" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Box>
-            </Box>
-          </Paper>
-        </Grid>
-      </Grid>
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                  value={data.first_name}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="last_name"
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  autoFocus
+                  value={data.last_name}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="email"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  type="email"
+                  autoComplete="email"
+                  value={data.email}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="password"
+                  required
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  type="password"
+                  autoComplete="new-password"
+                  value={data.password}
+                  onChange={handleChange}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}>
+              Sign Up
+            </Button>
+            <Link href="/login" variant="body2">
+              Already have an account? Sign in
+            </Link>
+          </Box>
+        </Box>
+      </Container>
     </ThemeProvider>
   );
 };
