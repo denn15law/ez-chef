e48992f94f6b7af5376d387f927dcfbdbf278a4a
dennis law <denn15_law@live.ca>
2022-01-19 18:26:28 -0500

    refractor addRecipeToGroceries function to accept both api recipes and user created recipes

--- backend/routes/groceryList.js
+++ backend/routes/groceryList.js
@@ -5,11 +5,17 @@ const {
   getUserGroceryItems,
   getUserGroceryRecipes,
   deleteRecipeFromGroceryList,
+  addRecipeToGroceries,
 } = require("../controllers/groceryList");
 
 router.get("/ingredients/:user_id", getUserGroceryItems);
 router.get("/recipes/:user_id", getUserGroceryRecipes);
 
+// add recipe to grocery list
+router.post("/add/:user_id/:recipe_id", addRecipeToGroceries);
+
 router.delete("/recipes/:user_id/:recipe_id", deleteRecipeFromGroceryList);
+
+// router.get("/test/:user_id/:recipe_id", addRecipeToGroceries);
 
 module.exports = router;
