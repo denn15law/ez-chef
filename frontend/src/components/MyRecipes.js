cda7bddafa80e6aba54dbdaf68874db17e10dcca
Jeewon Lee <jeewon.jlee@gmail.com>
2022-01-19 20:19:44 -0500

    Fix spacing between navbar and content

--- frontend/src/components/MyRecipes.js
+++ frontend/src/components/MyRecipes.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {
+  Box,
   Button,
   Card,
   CardActions,
   CardContent,
   CardMedia,
+  CssBaseline,
   Grid,
   Link,
   Paper,
@@ -56,59 +58,71 @@ const MyRecipes = ({ user }) => {
   };
 
   return (
-    <Paper style={styles.paperContainer}>
-      <Grid>
-        <Typography variant="h5">My Recipes</Typography>
-        <Grid
-          container
-          p={5}
-          spacing={{ xs: 2, md: 7 }}
-          columns={{ xs: 4, sm: 8, md: 12 }}
-          direction="row"
-          justifyContent="center"
-          alignItems="center">
-          {myRecipes.length ? (
-            myRecipes.map((recip) => {
-              const url = `http://localhost:3000/myRecipes/${recip._id}`;
-              return (
-                <Grid item key={recip}>
-                  <Card
-                    sx={{
-                      height: "100%",
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}>
-                    <CardContent sx={{ flexGrow: 1 }}>
-                      <Link href={url}>{recip.title}</Link>
-                    </CardContent>
-                    <CardMedia
-                      key={recip._id}
-                      component="img"
-                      src={recip.image_url}
-                      alt="recipe"
-                      style={{ height: 250, width: 250 }}
-                    />
-                    <CardActions>
-                      <Button
-                        onClick={() => {
-                          deleteRecipe(recip._id);
-                        }}
-                        size="small">
-                        Delete
-                      </Button>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              );
-            })
-          ) : (
-            <Typography variant="h5">Add a Recipe</Typography>
-          )}
+    <Grid>
+      <CssBaseline />
+      <Paper style={styles.paperContainer}>
+        <Grid container spacing={4}>
+          <Box
+            component="main"
+            sx={{
+              p: 5,
+              flexGrow: 1,
+              height: "100vh",
+              overflow: "auto",
+            }}>
+            <Typography variant="h5">My Recipes</Typography>
+            <Grid
+              container
+              p={5}
+              spacing={{ xs: 2, md: 7 }}
+              columns={{ xs: 4, sm: 8, md: 12 }}
+              direction="row"
+              justifyContent="center"
+              alignItems="center">
+              {myRecipes.length ? (
+                myRecipes.map((recip) => {
+                  const url = `http://localhost:3000/myRecipes/${recip._id}`;
+                  return (
+                    <Grid item key={recip}>
+                      <Card
+                        sx={{
+                          height: "100%",
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}>
+                        <CardContent sx={{ flexGrow: 1 }}>
+                          <Link href={url}>{recip.title}</Link>
+                        </CardContent>
+                        <CardMedia
+                          key={recip._id}
+                          component="img"
+                          src={recip.image_url}
+                          alt="recipe"
+                          style={{ height: 250, width: 250 }}
+                        />
+                        <CardActions>
+                          <Button
+                            onClick={() => {
+                              deleteRecipe(recip._id);
+                            }}
+                            size="small">
+                            Delete
+                          </Button>
+                        </CardActions>
+                      </Card>
+                    </Grid>
+                  );
+                })
+              ) : (
+                <Typography variant="h5">Add a Recipe</Typography>
+              )}
+            </Grid>
+          </Box>
         </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </Grid>
   );
 };
 
