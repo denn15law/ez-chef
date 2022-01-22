03c772bbddf4904f50755da0bfa39167b1486f0a
Jeewon Lee <jeewon.jlee@gmail.com>
2022-01-22 14:57:28 -0500

    Style text phone number box

--- frontend/src/components/RecipeDetailsCard.jsx
+++ frontend/src/components/RecipeDetailsCard.jsx
@@ -15,7 +15,6 @@ import StarIcon from "@mui/icons-material/Star";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useNavigate } from "react-router-dom";
 import { Alert } from "react-alert";
 import { textAlign } from "@mui/system";
