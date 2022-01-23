2d01fdbb11559c1bfac05d0f23d24d4c1e0e5508
Kyle <kliang1194@gmail.com>
2022-01-23 18:31:38 -0500

    remove unused controller functions from users backend

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -15,69 +15,7 @@ const getUserById = (req, res) => {
     .catch((err) => res.status(404).json({ message: err.message }));
 };
 
-const getUserCreatedRecipes = (req, res) => {
-  const user_id = req.params.user_id;
-  User.findById(user_id)
-    .then((user) => res.json(user.recipes_created))
-    .catch((err) => res.json(err.message));
-};
-
-const addRecipeToUser = (req, res) => {
-  const user_id = req.params.user_id;
-  const recipe_id = req.params.recipe_id;
-
-  Recipe.findById(recipe_id)
-    .then((recipe) => {
-      return User.findByIdAndUpdate(
-        user_id,
-        { $push: { groceryList_recipes: recipe._id } },
-        { new: true, upsert: true },
-        function (err, managerparent) {
-          if (err) throw err;
-          console.log(managerparent);
-        }
-      );
-    })
-    .then((updatedUser) => {
-      res.json(updatedUser);
-    })
-    .catch((err) => res.json(err));
-};
-
-const getUserFavourites = (req, res) => {
-  const user_id = req.params.user_id;
-  User.findById(user_id)
-    .then((user) => res.json(user.favourited_recipes))
-    .catch((err) => res.json(err.message));
-};
-
-const addFavouriteToUser = (req, res) => {
-  const user_id = req.params.user_id;
-  const favourite_id = req.params.favourite_id;
-
-  Favourite.findById(favourite_id)
-    .then((favourite) => {
-      return User.findByIdAndUpdate(
-        user_id,
-        { $push: { favourited_recipes: favourite._id } },
-        { new: true, upsert: true },
-        function (err, managerparent) {
-          if (err) throw err;
-          console.log(managerparent);
-        }
-      );
-    })
-    .then((updatedUser) => {
-      res.json(updatedUser);
-    })
-    .catch((err) => res.json(err));
-};
-
 module.exports = {
   getAllUsers,
   getUserById,
-  getUserCreatedRecipes,
-  addRecipeToUser,
-  getUserFavourites,
-  addFavouriteToUser,
 };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,27 +2,11 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-const {
-  getAllUsers,
-  getUserById,
-  getUserCreatedRecipes,
-  addRecipeToUser,
-  getUserFavourites,
-  addFavouriteToUser,
-} = require("../controllers/users");
+const { getAllUsers, getUserById } = require("../controllers/users");
 const { route } = require("express/lib/application");
 
 //Route: /users
 router.get("/", getAllUsers);
 router.get("/:id", getUserById);
-
-//Routes for user recipes
-router.get("/:user_id/recipes", getUserCreatedRecipes);
-
-// add Recipe to grocerylist
-router.post("/:user_id/grocery/:recipe_id", addRecipeToUser);
-
-router.get("/:user_id/favourites", getUserFavourites);
-router.post("/:user_id/:favourite_id", addFavouriteToUser);
 
 module.exports = router;
